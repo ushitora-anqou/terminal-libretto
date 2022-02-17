b86579e4eade058ecc7463d05b037f7f30b50679
Ushitora Anqou <ushitora@anqou.net>
2022-02-17 16:52:33 +0900

    wip

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/ushitora-anqou/terminal-libretto
+
+go 1.17
+
+require github.com/alessio/shellescape v1.4.1 // indirect
---
+++ main.go
@@ -0,0 +1,107 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"log"
+	"os"
+	"os/exec"
+	"time"
+
+	"github.com/alessio/shellescape"
+)
+
+type ScriptGenerator struct {
+	out                                              io.WriteCloser
+	breakPerChar, breakPerLineBegin, breakPerLineEnd time.Duration
+}
+
+func NewScriptGeneratorWithTempFile() (*ScriptGenerator, string, error) {
+	scriptFile, err := ioutil.TempFile("", "")
+	if err != nil {
+		return nil, "", err
+	}
+	scriptFilePath := scriptFile.Name()
+
+	gen := &ScriptGenerator{
+		out:               scriptFile,
+		breakPerChar:      100 * time.Millisecond,
+		breakPerLineBegin: 500 * time.Millisecond,
+		breakPerLineEnd:   500 * time.Millisecond,
+	}
+	return gen, scriptFilePath, nil
+}
+
+func (gen *ScriptGenerator) pr(str string) {
+	fmt.Fprintf(gen.out, "echo -n %s\n", shellescape.Quote(str))
+}
+
+func (gen *ScriptGenerator) newline() {
+	fmt.Fprintf(gen.out, "echo\n")
+}
+
+func (gen *ScriptGenerator) sleep(dur time.Duration) {
+	fmt.Fprintf(gen.out, "sleep %f\n", dur.Seconds())
+}
+
+func (gen *ScriptGenerator) displayLine(line string) {
+	gen.pr("$ ")
+	gen.sleep(gen.breakPerLineBegin)
+	for _, char := range line {
+		gen.pr(string(char))
+		gen.sleep(gen.breakPerChar)
+	}
+	gen.sleep(gen.breakPerLineEnd)
+	gen.newline()
+}
+
+func (gen *ScriptGenerator) execLine(line string) {
+	fmt.Fprintf(gen.out, "%s\n", line)
+}
+
+func (gen *ScriptGenerator) Close() {
+	gen.out.Close()
+}
+
+func (gen *ScriptGenerator) ReadLibretto(r io.Reader) {
+	scanner := bufio.NewScanner(r)
+	for scanner.Scan() {
+		line := scanner.Text()
+
+		gen.displayLine(line)
+		gen.execLine(line)
+	}
+}
+
+func run() error {
+	file, err := os.Open("libretto")
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	gen, tmpFilePath, err := NewScriptGeneratorWithTempFile()
+	if err != nil {
+		return err
+	}
+	defer os.Remove(tmpFilePath)
+	gen.ReadLibretto(file)
+	gen.Close()
+
+	cmd := exec.Command("bash", tmpFilePath)
+	cmd.Stdout = os.Stdout
+	cmd.Stdin = os.Stdin
+	cmd.Stderr = os.Stderr
+	cmd.Run()
+
+	return nil
+}
+
+func main() {
+	err := run()
+	if err != nil {
+		log.Fatalf("ERROR: %v", err)
+	}
+}
